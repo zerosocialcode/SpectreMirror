94558169650b586d59436d6b5faf5416a283ee7c
zerosocialcode <zerosocialcode@gmail.com>
2026-05-11 16:15:13 +0600

    refactor: add advanced-cloner option to CLI

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { program } = require('commander');
-const { cloneWebsite } = require('./cloner');
+const { cloneWebsite } = require('./advanced-cloner');
 const pkg = require('../package.json');
 const log = require('./logger');
 
@@ -12,6 +12,7 @@ program
   .requiredOption('-o, --output <directory>', 'Output directory for the cloned site')
   .option('-c, --chromium <path>', 'Path to Chromium/Chrome binary', '/usr/bin/chromium')
   .option('--no-assets', 'Do not download assets (images, CSS, JS)')
+  .option('--keep-external', 'Keep external links as-is (do not modify)')
   .option('-v, --verbose', 'Enable verbose logging')
   .parse(process.argv);
 
@@ -28,6 +29,7 @@ if (program.opts().verbose) {
       outputDir: program.opts().output,
       chromiumPath: program.opts().chromium,
       downloadAssets: program.opts().assets,
+      keepExternalLinks: program.opts().keepExternal,
     });
     log.info('✅ Cloning completed successfully!');
   } catch (err) {
